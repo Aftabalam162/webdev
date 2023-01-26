102fb99b3fea48aa3873eb153006990b7b215bd7
aftabalam162 <itsaftabalam37@gmail.com>
2023-01-26 18:11:04 +0530

    add preview feature and add post temporarily

--- instafeed/index.js
+++ instafeed/index.js
@@ -1,3 +1,4 @@
+var postCount = 1;
 var input = document.getElementById("image-input");
 var preview = document.getElementById("image-preview");
 var preview_box = document.getElementById("preview");
@@ -18,4 +19,28 @@ function previewImage() {
     if (file) {
         reader.readAsDataURL(file);
     }
+}
+
+function postImage() {
+    var image = input.files[0];
+    var reader = new FileReader();
+    reader.addEventListener("load", function() {
+        addPost(reader);
+    }, false);
+
+    if (image) {
+        reader.readAsDataURL(image);
+    }
+    
+    
+}
+
+function addPost(reader) {
+    var newNode = document.createElement("div");
+    newNode.classList.add("postNode");
+    newNode.innerHTML = `<h2>Post #${postCount++}:</h2> <img src="${reader.result}"/> <p id="image-details">${document.getElementById("photo-caption").value}</p>`;
+    document.querySelector(".profile-feed").appendChild(newNode);
+
+    form.reset();
+    preview_box.style.visibility = "collapse";
 }
