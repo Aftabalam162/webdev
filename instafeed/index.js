4084a4d70de1ade1ddf9053fa2a4330b5e10ffa8
aftabalam162 <itsaftabalam37@gmail.com>
2023-01-26 11:09:31 +0530

    added form reset on reload

--- instafeed/index.js
+++ instafeed/index.js
@@ -1,12 +1,18 @@
+var input = document.getElementById("image-input");
+var preview = document.getElementById("image-preview");
+var preview_box = document.getElementById("preview");
+var form = document.getElementById("upload-details");
+
+form.reset();
+
 function previewImage() {
-    var input = document.getElementById("image-input");
-    var preview = document.getElementById("image-preview");
 
     var file = input.files[0];
     var reader = new FileReader();
 
     reader.addEventListener("load", function() {
         preview.src = reader.result;
+        preview_box.style.visibility = "visible";
     }, false);
 
     if (file) {
