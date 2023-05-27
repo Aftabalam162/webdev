cdca0ccbb336e4ce3cab2a6731376a810e240163
aftabalam162 <itsaftabalam37@gmail.com>
2023-05-27 21:05:21 +0530

    begins ecommerce project

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Navbar } from './nav'
+import { Carousel } from './carousel.jsx'
+
+
+function App() {
+
+  return (
+    <>
+
+      <Navbar />
+      <Carousel/>
+    </>
+  )
+}
+
+
+export default App
